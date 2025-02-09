70ce8e1b4ad5b20951b90cd4b34adc3ed04cf0a9
JRBros <jrbros2346@gmail.com>
2025-02-10 03:18:49 +0530

    [bank-heist]

--- bank-heist/heist.py
+++ bank-heist/heist.py
@@ -18,7 +18,7 @@ def md5(secret, msg):
 def menu(secret):
     while True:
         print("\n1. Practice Convo")
-        print("2. Let's Fool Alice!")
+        print("2. Let's Fool Hackerz!")
         print("3. Crack the Vault")
         print("4. Exit")
         try:
@@ -28,7 +28,7 @@ def menu(secret):
         if choice == '1':
             practice_convo(secret)
         elif choice == '2':
-            fool_alice(secret)
+            fool_hackerz(secret)
         elif choice == '3':
             crack_the_vault()
         elif choice == '4':
@@ -47,40 +47,40 @@ def practice_convo(secret):
     print(f"Here is your encrypted message: {hash}")
 
 
-def fool_alice(secret):
-    print("\nBot: Okay, let's see if you're the real deal. What's your name?")
+def fool_hackerz(secret):
+    print("\nTrojan: Okay, let's see if you're the real deal. What's your name?")
     try:
         user_name = input("Your name: ").encode('latin-1')
     except EOFError:
         exit(0)
     user_name = user_name.decode('unicode_escape').encode('latin-1')
-    print("\nBot: Please provide your HMAC")
+    print("\nTrojan: Please provide your HMAC")
     try:
         user_hmac = input("Your HMAC: ").encode('latin-1')
     except EOFError:
         exit(0)
 
-    if b"Bob" in user_name:
+    if b"Revil" in user_name:
         hash = base64.b64decode(md5(secret, user_name))
         if user_hmac == hash:
-            print("\nAlice: Oh hey Bob! Here is the vault code you wanted:")
-            with open('secret.txt', 'r') as file:
+            print("\nHackerz: Oh hey Revil! Here is the vault code you wanted:")
+            with open('secret-key', 'r') as file:
                 secret_content = file.read()
                 print(secret_content)
         else:
-            print("\nAlice: LIARRRRRRR!!")
+            print("\nHackerz: LIARRRRRRR!!")
     else:
-        print("\nAlice: IMPOSTERRRR")
+        print("\nHackerz: IMPOSTERRRR")
 
 
 def crack_the_vault():
-    print("\nVault Person: Enter password")
+    print("\nTalos: Enter password")
     try:
         passs = input("Password: ")
     except EOFError:
         exit(0)
 
-    with open('secret.txt', 'r') as file:
+    with open('secret-key', 'r') as file:
         secret_content = file.read().strip()
         if passs == secret_content:
             with open('flag') as f:
